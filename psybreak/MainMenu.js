0849cae1a280dafaae8a5f4f630e8b1b1e3f1122
Nathaniel <norchis79@gmail.com>
2019-04-18 16:35:57 +1000

    changes 18/04/2019

--- psybreak/MainMenu.js
+++ psybreak/MainMenu.js
@@ -9,23 +9,23 @@ DoofBreak.MainMenu.prototype = {
     
         // This function will be executed at the beginning     
         // That's where we load the images and sounds 
-         this.load.image('paddle', 'assets/e.jpg');
-         this.load.image('bullet', 'assets/bullet2.png');
-         this.load.image('brick', 'assets/brick.png');
-         this.load.image('straight_line', 'assets/straight_line.png');
+         this.load.image('paddle', './assets/e.jpg');
+         //this.load.image('bullet', './assets/bullet2.png');
+         this.load.image('brick', './assets/brick.png');
+         this.load.image('straight_line', './assets/straight_line.png');
          
 
-        this.game.load.image('bimg', 'assets/bimg1.jpg');
-        this.game.load.image('start_game', 'assets/start_game.jpg');
-        this.game.load.image('credits', 'assets/credits_button.jpg');
-        this.game.load.image('instructions', 'assets/instructions_button.jpg');
+        this.game.load.image('bimg', './assets/backgrounds/bimg1.jpg');
+        this.game.load.image('start_game', './assets/start_game.jpg');
+        //this.game.load.image('credits', './assets/credits_button.jpg');
+        //this.game.load.image('instructions', './assets/instructions_button.jpg');
 
         
        
     },
 
     create: function() { 
-     	m1=this.game.add.audio('m1');
+     	//m1=this.game.add.audio('m1');
    	//m1.play();
    	this.game.stage.backgroundColor = '#000000';
   	  	this.game.add.image(this.game.world.centerX, this.game.world.centerY, 'bimg').anchor.set(0.5);
@@ -33,8 +33,8 @@ DoofBreak.MainMenu.prototype = {
     	var style = { font: "16px Helvetica", fill: "#FFFFFF", align: "right" };
     	
     	 button_2back1 = this.game.add.button(240,80, 'start_game', this.start_game, this);
-    	 button_2back2 = this.game.add.button(240,160, 'instructions', this.instructions, this);
-    	 button_2back4 = this.game.add.button(240,320, 'credits', this.start_credits, this);
+    	 //button_2back2 = this.game.add.button(240,160, 'instructions', this.instructions, this);
+    	 //button_2back4 = this.game.add.button(240,320, 'credits', this.start_credits, this);
   
     },
   	 update: function() {	
@@ -45,10 +45,10 @@ DoofBreak.MainMenu.prototype = {
   	 	 this.game.state.start('Game');
   	 },
   	 start_credits: function() {
-  	 	this.game.state.start('Credits');
+  	 	//this.game.state.start('Credits');
   	 },
   	instructions: function() {
-  	 	this.game.state.start('Instructions');
+  	 	//this.game.state.start('Instructions');
   	 }
  }
 ;
